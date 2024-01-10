774724c508094c82b9caed631936ac13d09bc2e4
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2024-01-10 11:51:17 -0300

    feat: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,69 @@
-import React from "react";
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
-const Navbar = () => <div>Navbar</div>;
+const Navbar = () => {
+  const liClasses = `
+    font-poppins
+    font-normal
+    cursor-pointer
+    text-[16px]
+    text-white
+  `;
+  const [toggle, setToggle] = useState(false);
+
+  const Link = ({ nav }) => <a href={`#${nav.id}`}>{nav.title}</a>;
+
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`
+              ${liClasses}
+              ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}
+            `}
+          >
+            <Link nav={nav} />
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`
+            ${toggle ? "flex" : "hidden"}
+            p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar
+          `}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`
+                  ${liClasses}
+                  justify-center
+                  ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}
+                `}
+              >
+                <Link nav={nav} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
