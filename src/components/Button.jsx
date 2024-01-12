37c45e908b152058d884a2fc06ba7af6b4888fd3
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2024-01-12 13:40:34 -0300

    feat: Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-
-const Button = () => <div>Button</div>;
+const Button = ({ styles, children }) => (
+  <button
+    type="button"
+    className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none capitalize ${styles}`}
+  >
+    {children}
+  </button>
+);
 
 export default Button;
