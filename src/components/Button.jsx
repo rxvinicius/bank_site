ea8e87113c7099a30f523c4f796614788ccc4065
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2024-01-13 11:05:58 -0300

    style: Button component rounded

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
 const Button = ({ styles, children }) => (
   <button
     type="button"
-    className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none capitalize ${styles}`}
+    className={`
+      py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none capitalize rounded-[10px]
+      ${styles}
+    `}
   >
     {children}
   </button>
